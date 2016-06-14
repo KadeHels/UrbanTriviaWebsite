9f003ae3450fa2f2c43c98ca3e309108d5443467
HELSETK <HELSETK@SP102201>
2016-06-14 15:36:50 -0500

    Updated so much. Used angular to traverse pages on the client side slowly weeding out the Razor code left. added two pages: wordListView and the wordsView.
    wordListView displays all the words in the dictionary.
    
    wordsView is the framework of the game page.

--- WebApplication2/js/home-index.js
+++ WebApplication2/js/home-index.js
@@ -1,7 +1,37 @@
 ﻿//home-index.js
-var app = angular.module("myapp", []);
+var module = angular.module("homeIndex", ['ngRoute']);
 
-app.controller("homeIndexController", function ($scope, $http) {
+module.config(function ($routeProvider){
+    $routeProvider.when("/", {
+        controller: "wordsController",
+        templateUrl: "/Templates/wordsView.html"
+    });
+    $routeProvider.when("/wordList", {
+        controller: "wordListController",
+        templateUrl: "Templates/wordListView.html"
+    });
+
+    $routeProvider.otherwise({ redirectTO: "/" });
+});
+module.controller("wordListController", function ($scope, $http) {
+    $scope.data = [];
+    $scope.isBusy = true;
+
+    $http.get("../api/words")
+    .then(function (result) {
+        // success
+        angular.copy(result.data, $scope.data);
+    },
+    function () {
+        // error
+        alert("could not load words");
+    })
+    .then(function () {
+        $scope.isBusy = false;
+    });
+});
+
+module.controller("wordsController", function ($scope, $http) {
     
     $scope.data = [];
     $scope.isBusy = true;
@@ -9,8 +39,7 @@ app.controller("homeIndexController", function ($scope, $http) {
     $http.get("/api/words")
     .then(function (result) {
         // success
-        angular.copy(result.data, $scope.data);
-       
+        angular.copy(result.data, $scope.data);      
     },
     function () {
         // error
